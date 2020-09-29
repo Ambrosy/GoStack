70def405ce32e22641bc86917e2c2ada094155ae
Rafael Souza Ambrosi <leafar2010@gmail.com>
2020-09-29 18:02:24 -0300

    created assets and update

--- 02_Front-end_com_ReactJS/frontend/src/App.js
+++ 02_Front-end_com_ReactJS/frontend/src/App.js
@@ -1,5 +1,7 @@
 import React,{ useState } from 'react';
 import "./App.css";
+import backgroundImage from "./assets/background.jpeg";
+
 import Header from "./components/Header";
 
 function App(){
@@ -14,6 +16,9 @@ function App(){
     return (
     <> 
     <Header title="Projects"/>
+
+    <img width={300} src={backgroundImage}/>
+
     <ul>
     {projects.map(project=> <li key={project}>{project}</li>,)}
     </ul>
