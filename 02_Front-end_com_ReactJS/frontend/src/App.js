d25b69e70bfcd5faa4c1e2a2d67ab84f433f4484
Rafael Souza Ambrosi <leafar2010@gmail.com>
2020-09-29 13:25:59 -0300

    created file App.css and update

--- 02_Front-end_com_ReactJS/frontend/src/App.js
+++ 02_Front-end_com_ReactJS/frontend/src/App.js
@@ -1,14 +1,14 @@
 import React,{ useState } from 'react';
-
+import "./App.css";
 import Header from "./components/Header";
 
 function App(){
     const [projects,setProjects] = useState(['Desenvolvimento de app','Front-end web']);
     
     function handleAddProject(){
-        projects.push(`Novo projeto ${Date.now()}`);
-        //setProjects
-        console.log(projects);
+        //projects.push(`Novo projeto ${Date.now()}`);
+        
+        setProjects([...projects,`Novo projeto ${Date.now()}`]);
     }
     
     return (
